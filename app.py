6d904ecf75cf17f4f1630bb0319c0aaacb528df9
sanjayy01 <sanjaynareshc@gmail.com>
2024-04-02 14:47:29 +0530

    .

--- app.py
+++ app.py
@@ -1,40 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, request, jsonify,render_template
+
+from models import *
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"
 
-# Sample product data (you can replace it with your own database)
-products = [
-    {"id": 1, "name": "Product 1", "price": 10},
-    {"id": 2, "name": "Product 2", "price": 20},
-    {"id": 3, "name": "Product 3", "price": 30}
-]
 
 @app.route('/')
-def index():
-    return render_template('index.html', products=products)
+def signup():
+    return render_template("signup.html")
+   
+@app.route('/get_signup',methods=['GET','POST'])
+def get_signup():
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    return jsonify({"users":users})
 
-@app.route('/add_to_cart/<int:product_id>')
-def add_to_cart(product_id):
-    product = next((p for p in products if p['id'] == product_id), None)
-    if product:
-        if 'cart' not in session:
-            session['cart'] = []
-        session['cart'].append(product)
-        return redirect(url_for('index'))
+   
+
+@app.route('/post_signup', methods=['POST'])
+def post_signup():
+    if request.method == "POST":
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        add_user(username, email, password)
+        return jsonify({"message": "Welcome, {}!".format(username)})
     else:
-        return "Product not found", 404
+        return "Method Not Allowed", 405
 
-@app.route('/cart')
-def view_cart():
-    cart = session.get('cart', [])
-    total_price = sum(item['price'] for item in cart)
-    return render_template('cart.html', cart=cart, total_price=total_price)
-
-@app.route('/checkout')
-def checkout():
-    session.pop('cart', None)
-    return "Checkout successful!"
+@app.route('/delete')
+def delete_userS():
+    try:
+        id=3
+        delete_user(id)
+        return "delete"
+    except:
+        return "no"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
