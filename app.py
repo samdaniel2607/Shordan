7602645be811281d56b48a012ce60230da2dc03a
sanjayy01 <sanjaynareshc@gmail.com>
2024-04-22 23:30:37 +0530

    app commit

--- app.py
+++ app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def index():
+    return render_template('signup.html')
+
+@app.route('/login')
+def index():
+    return render_template('login.html')
+
+@app.route('/shoedetails')
+def index():
+    return render_template('shoedetails.html')
+@app.route('/shop')
+def index():
+    return render_template('shop.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
